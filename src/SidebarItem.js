7d7e69c330ee5e96a4995a50a0d7f2ff41007e4b
Brian Mock <brian@mockbrian.com>
2017-03-12 10:11:46 -0700

    Even more Slack-y

--- src/SidebarItem.js
+++ src/SidebarItem.js
@@ -22,22 +22,56 @@ SidebarItem.propTypes = {
 const OFFLINE = '\u25CB';
 const ONLINE = '\u25CF';
 
+const style = {
+  minWidth: '200px',
+};
+
 function renderPerson(person, i) {
   const isOnline = i % 5 <= 2;
   if (isOnline) {
-    return <div key={i} className='pointer br2 br--right pv1 hover-bg-white-20 o-70 pl3'>{ONLINE} {person}</div>;
+    return (
+      <div
+        style={style}
+        key={i}
+        className='pointer br2 br--right pv1 hover-bg-white-20 o-70 pl3'
+      >{ONLINE} {person}</div>
+    );
   } else {
-    return <div key={i} className='pointer br2 br--right pv1 hover-bg-white-20 pl3 o-70'>{OFFLINE} {person}</div>;
+    return (
+      <div
+        style={style}
+        key={i}
+        className='pointer br2 br--right pv1 hover-bg-white-20 pl3 o-70'
+      >{OFFLINE} {person}</div>
+    );
   }
 }
 
 function renderChannel(channel, i) {
   if (i === 3) {
-    return <div key={i} className='pointer br2 br--right pv1 pl3 bg-black-20'># {channel}</div>;
+    return (
+      <div
+        style={style}
+        key={i}
+        className='pointer br2 br--right pv1 pl3 bg-black-20'
+      ># {channel}</div>
+    );
   } else if (i % 4 === 0) {
-    return <div key={i} className='pointer br2 br--right pv1 hover-bg-white-20 pl3 b'># {channel}</div>;
+    return (
+        <div
+        style={style}
+        key={i}
+        className='pointer br2 br--right pv1 hover-bg-white-20 pl3 b'
+      ># {channel}</div>
+    );
   } else {
-    return <div key={i} className='pointer br2 br--right pv1 hover-bg-white-20 pl3 o-70'># {channel}</div>;
+    return (
+        <div
+        style={style}
+        key={i}
+        className='pointer br2 br--right pv1 hover-bg-white-20 pl3 o-70'
+      ># {channel}</div>
+    );
   }
 }
 
