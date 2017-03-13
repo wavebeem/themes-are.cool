3a4cb81c1af765c9793dadd1d49836ba6c4187a1
Brian Mock <brian@mockbrian.com>
2017-03-12 20:20:20 -0700

    Actually generate a theme now

--- src/Configurator.js
+++ src/Configurator.js
@@ -1,4 +1,5 @@
 import React, {Component, PropTypes as PT} from 'react';
+import TinyColor from 'tinycolor2';
 
 class Configurator extends Component {
   render() {
@@ -8,6 +9,21 @@ class Configurator extends Component {
       onChangePrimaryColor,
       onChangeThemeType
     } = this.props;
+    // TODO: Move this state to the app and pass it to the sidebar.
+    const badgeColor = '#fc6769';
+    const lighterColor = TinyColor(primaryColor).lighten(8).toString();
+    const darkerColor = TinyColor(primaryColor).darken(8).toString();
+    const foregroundColor = themeType === 'dark' ? '#ffffff' : '#000000';
+    const theme = [
+      primaryColor, // Column BG
+      lighterColor, // Menu BG Hover
+      darkerColor, // Active Item
+      foregroundColor, // Active Item Text
+      lighterColor, // Hover Item
+      foregroundColor, // Text Color
+      foregroundColor, // Active Presence
+      badgeColor, // Mention Badge
+    ].join(',');
     return (
       <div className='flex-auto pb4'>
         <h1 className='bb ph3 pv2 b--black-20 db mt0'>Slack Themes are Cool</h1>
@@ -15,12 +31,12 @@ class Configurator extends Component {
           <div className='ttu b mb1'>Color</div>
           <input
             type='text'
-            className='ba b--black-20 br1 pa1 mb3'
+            className='ba b--black-20 br1 pa1 mb3 code'
             value={primaryColor}
             onChange={onChangePrimaryColor}
           />
         </label>
-        <div className='db ph3'>
+        <div className='db ph3 mb3'>
           <label className='db lh-copy'>
             <input
               type='radio'
@@ -29,7 +45,7 @@ class Configurator extends Component {
               checked={themeType === 'dark'}
               onChange={onChangeThemeType}
             />
-            <span className='ml2'>Dark background</span>
+            <span className='ml2'>Light text</span>
           </label>
           <label className='db lh-copy'>
             <input
@@ -39,9 +55,18 @@ class Configurator extends Component {
               checked={themeType === 'light'}
               onChange={onChangeThemeType}
             />
-            <span className='ml2'>Light background</span>
+            <span className='ml2'>Dark text</span>
           </label>
         </div>
+        <label className='db ph3'>
+          <div className='b ttu mb1'>
+            Copy and paste this into Slack
+          </div>
+          <input
+            value={theme}
+            className='w-100 ba b--black-20 br1 pa1 mb3 code'
+          />
+        </label>
       </div>
     );
   }
