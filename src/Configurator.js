9ee1bb1f621e51f9001a83d8f5f2b8fe51a0a690
Brian Mock <brian@mockbrian.com>
2017-03-12 20:37:35 -0700

    Stuff

--- src/Configurator.js
+++ src/Configurator.js
@@ -24,20 +24,31 @@ class Configurator extends Component {
       foregroundColor, // Active Presence
       badgeColor, // Mention Badge
     ].join(',');
+    const selectAll = event => {
+      event.target.focus();
+      event.target.select();
+    };
     return (
       <div className='flex-auto pb4'>
-        <h1 className='bb ph3 pv2 b--black-20 db mt0'>Slack Themes are Cool</h1>
+        <h1 className='bb ph3 pv2 b--black-20 db mt0'>
+          Slack Themes are Cool
+        </h1>
         <label className='db ph3'>
-          <div className='ttu b mb1'>Color</div>
+          <h2 className='ttu b f6 mt0 mb1'>
+            Color
+          </h2>
           <input
             type='text'
             className='ba b--black-20 br1 pa1 mb3 code'
+            onFocus={selectAll}
             value={primaryColor}
             onChange={onChangePrimaryColor}
           />
         </label>
         <div className='db ph3 mb3'>
-          <div className='db b ttu'>Text color</div>
+          <h2 className='db b ttu f6 mv0'>
+            Text color
+          </h2>
           <label className='db lh-copy'>
             <input
               type='radio'
@@ -60,10 +71,12 @@ class Configurator extends Component {
           </label>
         </div>
         <label className='db ph3'>
-          <div className='b ttu mb1'>
+          <h2 className='b ttu f6 mt0 mb1'>
             Copy and paste this into Slack
-          </div>
+          </h2>
           <input
+            onFocus={selectAll}
+            onChange={() => {}}
             value={theme}
             className='w-100 ba b--black-20 br1 pa1 mb3 code'
           />
